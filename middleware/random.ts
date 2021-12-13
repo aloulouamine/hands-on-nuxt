b381a94836affde833917ac90848b71dd5d21562
Amine ALOULOU <amine.aloulou@nordnet.com>
2021-12-13 22:00:17 +0100

    feat: add delay to random middleware

--- middleware/random.ts
+++ middleware/random.ts
@@ -1,8 +1,12 @@
 import { Middleware } from '@nuxt/types'
 
 const randomMiddleware: Middleware = ({ store }) => {
-  console.log('Calculating random value in server.')
-  store.commit('random/SetValue', Math.floor(Math.random() * 10))
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      store.commit('random/SetValue', Math.floor(Math.random() * 10))
+      resolve()
+    }, 2000)
+  )
 }
 
 export default randomMiddleware
